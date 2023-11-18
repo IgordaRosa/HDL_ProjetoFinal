969126d27785c0fa37b4377895ce8f0cc5488e8e
Igor da Rosa <igordematosdarosa@gmail.com>
2023-11-18 14:41:37 -0300

    rework

--- src/vendingmachine.v
+++ src/vendingmachine.v
@@ -1,96 +1,91 @@
-module vendingmachine(
-  input clk,
-  input wire rst,
-  input wire [3:0] Linha,
-  input wire [3:0] Coluna,
-  output wire rele,
-  output reg [3:0] numero,
-  input wire sensor1, 
-  input wire sensor2
-);
-  reg [3:0] tecla_anterior;
-  reg [3:0] tecla_atual;
-  reg [7:0] debounce_count;
-  reg [3:0] contador_sensor1;
-  reg [3:0] contador_sensor2;
-  reg [1:0] estado_sensor1;
-  reg [1:0] estado_sensor2;
-  
-  parameter MAX_COUNT = 8'd10;
+module vendingmachine(reset, clock, coluna, linha, sensor1, sensor2,rele);
+input wire [2:0] coluna;
+input wire [3:0] linha;
+input wire sensor1, sensor2;
+input clock, reset;
+output wire rele;
+reg girar;
+reg [3:0] contador;
+reg [3:0] contadorSensores;
+reg NumGiro;
+reg [6:0] BackupTecla;
+reg esperar;
 
-  always @(posedge clk or posedge rst) begin
-    if (rst) begin
-      tecla_anterior <= 4'b1111;
-      tecla_atual <= 4'b1111;
-      debounce_count <= 8'b0;
-      numero <= 4'b0000;
-		contador_sensor1 <= 4'b0000;
-      contador_sensor2 <= 4'b0000;
-      estado_sensor1 <= 2'b11;
-      estado_sensor2 <= 2'b11;
-    end else begin
-      if (Linha == 4'b1111) begin
-        tecla_anterior <= tecla_atual;
-        tecla_atual <= Coluna;
-      end
-		
-		if (sensor1 == estado_sensor1[0])
-      contador_sensor1 <= 4'b0000;
-      else if (contador_sensor1 < MAX_COUNT)
-      contador_sensor1 <= contador_sensor1 + 1;
-		
-		estado_sensor1 <= {sensor1, estado_sensor1[0]};
-
-		// Se o contador atingir MAX_COUNT, atualize o número
-		if (contador_sensor1 == MAX_COUNT && estado_sensor1[1] == 1'b0)begin
-       if(numero != 4'b0000)begin
-		  numero <= numero - 4'b0001;
-		 end
-		end
-		
-		// Lógica para debouncing do sensor2
-		if (sensor2 == estado_sensor2[0])
-      contador_sensor2 <= 4'b0000;
-		else if (contador_sensor2 < MAX_COUNT)
-      contador_sensor2 <= contador_sensor2 + 1;
-		estado_sensor2 <= {sensor2, estado_sensor2[0]};
-
-		// Se o contador atingir MAX_COUNT, atualize o número
-		if (contador_sensor2 == MAX_COUNT && estado_sensor2[1] == 1'b0)
-      numero <= numero - 4'b0001;
-		
-      if (tecla_anterior != tecla_atual) begin
-        debounce_count <= debounce_count + 1;
-        if (debounce_count == 8'hFF) begin
-          numero <= mapear_tecla(tecla_atual);
-          debounce_count <= 8'b0;
-        end
-      end else begin
-        debounce_count <= 8'b0;
-      end
+always @(posedge clock or posedge reset)begin
+ if(reset)begin
+  NumGiro = 4'b0000;
+  contador = 4'b0000;
+  girar = 1'b0;
+  BackupTecla = 8'b0;
+  esperar = 1'b0;
+ end
+ else begin
+  if(esperar == 1'b0)begin
+   if(contador == 4'b1111)begin
+   // define o valor de numGiro para qual tecla do teclado matricial foi pressionada.
+    if(linha == 4'b0001)begin
+    // codigo para deletar do display e confimar, 0 reseta o numero.
+	
     end
-  end
-  
-  
-assign rele = ((numero != 4'b0000) || (sensor1 == 1'b0 && sensor2 == 1'b0)) ? 1'b1 : 1'b0;
+    else if(coluna == 3'b100) begin
+     NumGiro = 4'b0001;
+	 end
+    else if(coluna == 3'b010) begin
+     NumGiro = 4'b0010;
+	 end
+    else if(coluna == 3'b001) begin
+     NumGiro = 4'b0011;
+	 end
 
-  
-  
-  /*
-  always @(posedge sensor1 or posedge sensor2)begin
-	if(sensor1 == 1'b0 or sensor2 == 1'b0)begin
-	 numero = numero - 4'b0001;
-	end
-  end*/
+    if(linha == 4'b0001 || linha == 4'b1000) begin
+    // não faz nada, só pra n cair na adição da linha caso esteja selecionando operação ou seja a primeira linha.
+	 end
+    else if(linha == 4'b0100) begin
+     NumGiro = NumGiro + 4'b0011;
+	 end
+    else if(linha == 4'b0010) begin
+     NumGiro = NumGiro + 4'b0110;
+	 end
+   end // if(contador == 4'b1111)begin
+  end // if(esperar == 1'b0)begin
+ 
+ if(NumGiro != 4'b0000)begin
+  girar = 1'b0;
+ end
+ else begin
+  girar = 1'b1;
+  esperar = 1'b1;
+ end
+ 
+ if(BackupTecla == {coluna, linha})begin
+  contador = contador + 4'b0001;
+ end
+ else begin
+  contador = 4'b0000;
+  BackupTecla = {coluna, linha};
+ end
+ 
+ if(sensor1 == 1'b1 && sensor2 == 1'b1)begin
+  contadorSensores = contadorSensores + 4'b0001;
+ end
+ else begin
+  contadorSensores = 4'b0000;
+ end
+ 
+ if(contadorSensores == 4'b1111)begin
+   contadorSensores = 4'b0000;
+   if(NumGiro != 4'b0000)begin
+	 NumGiro = NumGiro - 4'b0001;
+   end
+   else begin
+	 esperar = 1'b0;
+    girar = 1'b0;
+   end
+  end // if(contadorSensores == 4'b1111)begin
+ end // else do reset
+end // always
 
-  function [3:0] mapear_tecla;
-    input [3:0] coluna;
-    case (coluna)
-      4'b1110: mapear_tecla = 4'b0001; // Tecla 1
-      4'b1101: mapear_tecla = 4'b0010; // Tecla 2
-      4'b1011: mapear_tecla = 4'b0011; // Tecla 3
-      4'b0111: mapear_tecla = 4'b0100; // Tecla 4
-      default: mapear_tecla = 4'b0000; // Nenhuma tecla pressionada ou múltiplas teclas pressionadas
-    endcase
-  endfunction
+// F(ABC)=	girar	+ s1*s2: por que s1 e s2 são invertidos
+assign rele = girar | sensor1 & sensor2;
+
 endmodule
