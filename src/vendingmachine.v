25625a2ed747c2c5e80dcf75b77d2fb6a841150f
ribasnath <ribasnathalia9@gmail.com>
2023-11-23 00:45:57 -0300

    lcd

--- src/vendingmachine.v
+++ src/vendingmachine.v
@@ -1,4 +1,6 @@
-module vendingmachine(reset_in, clock_in, coluna_in, linha_in, sensor1_in, sensor2_in, rele_out, lcd_out, enlcd_out, rslcd_out, rwlcd_out);
+module vendingmachine(reset_in, clock_in, 
+							 coluna_in, linha_in, sensor1_in, sensor2_in, rele_out,
+							 lcd_out, enlcd_out, rslcd_out, rwlcd_out);
 
 input wire [2:0] coluna_in;
 input wire [3:0] linha_in;
@@ -20,6 +22,15 @@ output [7:0] lcd_out;
 output enlcd_out;
 output rslcd_out;
 output rwlcd_out;
+// Definir parâmetros do display
+localparam NUM_ROWS = 2;
+localparam NUM_COLS = 16;
+// Declaração de sinais internos
+reg [3:0] state;
+reg [5:0] row;
+reg [5:0] col;
+reg [7:0] display_ram[NUM_ROWS * NUM_COLS - 1:0];
+reg [7:0] NumGiroConv;
 
 always @(posedge clock_in or posedge reset_in)begin
  if(reset_in)begin
@@ -102,5 +113,41 @@ end // always
 
 // F(ABC)=	girar	+ s1*s2: por que s1 e s2 são invertidos
 assign rele_out = girar | (sensor1_in & sensor2_in);
+
+always @(posedge clock_in or posedge reset_in)begin
+	case (NumGiro)
+	 4'b0000: NumGiroConv = 8'b00110000; // '0' em ASCII
+	 4'b0001: NumGiroConv = 8'b00110001; // '1' em ASCII
+	 4'b0010: NumGiroConv = 8'b00110010; // '2' em ASCII
+	 4'b0011: NumGiroConv = 8'b00110011; // '3' em ASCII
+	 4'b0100: NumGiroConv = 8'b00110100; // '4' em ASCII
+	 4'b0101: NumGiroConv = 8'b00110101; // '5' em ASCII
+	 4'b0110: NumGiroConv = 8'b00110110; // '6' em ASCII
+	 4'b0111: NumGiroConv = 8'b00110111; // '7' em ASCII
+	 4'b1000: NumGiroConv = 8'b00111000; // '8' em ASCII
+	 4'b1001: NumGiroConv = 8'b00111001; // '9' em ASCII
+	 default: NumGiroConv = 8'b00110000; // Se não for um dígito, mostre '0'
+	endcase
+	
+	if (reset_in) begin
+		row <= 6'b0;
+		col <= 6'b0;
+	end
+	else if (enlcd_out) begin
+		display_ram[row * NUM_COLS + col] <= NumGiroConv;
+		col <= col + 1;
+		if (col == NUM_COLS) begin
+			col <= 6'b0;
+			row <= row + 1;
+			if (row == NUM_ROWS) begin
+				row <= 6'b0;
+			end
+		end
+  end
+  
+end
+
+assign rslcd_out = 1'b1;  // Modo de dados
+assign rwlcd_out = 1'b0;  // Leitura/gravação
 
 endmodule
